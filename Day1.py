52e625711596325ca8508ab281c7497a0e1df895
grahambx <graham.bance@gmail.com>
2024-05-06 15:57:33 +0100

    Day1 and Day2 added
    Basic concepts

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# Udemy 100 Projects in 100 days
+# Day 1 Project
+# Print -- Input -- Concatenation -- print double quotes
+print("Welcome to the band name generator!!!!")
+birth_place = input("Where were you born? \n")
+first_pet = input("What was you first pets name? \n")
+print('Your band name is: \n"' + first_pet + ' does ' + birth_place + '"' )
---
+++ Day2.py
@@ -0,0 +1,9 @@
+# Udemy 100 Projects in 100 days
+# Day 1 Project
+# Type Conversion -- F String
+print("Welcome to the tip calculator!")
+cost = float(input("What was the total bill? "))
+tip_rate = int(input("How much tip do you want to give? (5, 10, 15): "))
+customers = int(input("How many customers do we need to split the bill for? "))
+split = (cost + cost*tip_rate/100)/customers
+print(f"Each person pays: £{split:.2f}")
