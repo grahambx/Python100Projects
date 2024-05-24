0d0b85f7bbd3ce50301054f787f11332ea8e14ba
grahambx <graham.bance@gmail.com>
2024-05-24 11:41:45 +0100

    Day 18 added
    Library Practice, drawings with turtle

--- Scratch.py
+++ Scratch.py
@@ -1,6 +1,26 @@
-def test(a, b):
-    c = a + b
-    return c
+from turtle import Turtle, Screen
 
+geoff = Turtle()
+roger = Turtle()
+sleeves = Turtle()
+spokes = Turtle()
 
-c = test(1, 2)
+mutant_turtles = [geoff, roger, sleeves, spokes]
+
+geoff.color("red")
+roger.color("green")
+sleeves.color("blue")
+spokes.color("purple")
+
+geoff.left(90)
+roger.left(180)
+sleeves.right(90)
+
+for _ in range(4):
+    for turtle in mutant_turtles:
+        turtle.turtlesize(3,3,3)
+        turtle.forward(100)
+        turtle.left(90)
+
+screen = Screen()
+screen.exitonclick()
